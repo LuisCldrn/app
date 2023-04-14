28269f5f30ca3eb4270c4cbd75b9fa5a2aa43362
LuisCldrn <Luis.cldrn@yahoo.com>
2023-04-14 15:32:45 -0500

    updated links and resume link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'home', component: HomeComponent},
   {path: 'aboutme', component: AboutmeComponent, children: [{path: 'aboutme', component: AboutmeComponent}, {path: 'projects', component: ProjectsComponent}]}, 
   {path: 'projects', component: ProjectsComponent},
+  {path: 'contactme', component: ContactMeComponent},
   {path: 'test', component: HomeComponent}
 ];
 
